227a72aa3ed11b794c0295df0f32cc5caae6cd43
VladislavVik1 <Dakraman1232@ukr.net>
2025-06-16 18:41:25 +0300

    1

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -1,4 +1,4 @@
-import styles from './ImageModal.module.css';
+import Modal from 'react-modal';
 import type { ApiImage } from '../../types';
 
 type Props = {
@@ -6,8 +6,25 @@ type Props = {
   onClose: () => void;
 };
 
-export const ImageModal: React.FC<Props> = ({ image, onClose }) => (
-  <div className="modal" onClick={onClose}>
-    <img src={image.largeImageURL} alt={image.tags} />
-  </div>
-);
+Modal.setAppElement('#root'); // Важливо для доступності
+
+export const ImageModal: React.FC<Props> = ({ image, onClose }) => {
+  return (
+    <Modal
+      isOpen={!!image}
+      onRequestClose={onClose}
+      contentLabel="Image Modal"
+      style={{
+        content: {
+          maxWidth: '80%',
+          margin: 'auto',
+          padding: '0',
+          textAlign: 'center',
+        },
+      }}
+    >
+      <img src={image.largeImageURL} alt={image.tags} style={{ width: '100%' }} />
+      <button onClick={onClose}>Close</button>
+    </Modal>
+  );
+};
